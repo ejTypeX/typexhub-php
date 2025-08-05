9d6e7cbc2925645e0cae4e6b50502da1d53ac8e9
Ayrton <ayrtonsoaresm@gmail.com>
2025-08-05 18:44:51 -0300

    grid: styling global section, container and card, insert space with fake data in card

--- src/menu/presidencia/presidencia.php
+++ src/menu/presidencia/presidencia.php
@@ -3,5 +3,52 @@ include "../../include/header.php";
 include "../../include/sidebar.php";
 ?>
 
+<section id="section_avisos_presidencia">
+    <div id="avisos_presidencia" class="section">
+        <h1>Avisos</h1>
+        <div class="container">
+            <div class="card">
+                <button class="status-button">
+                    <span class="status-text">Atrasado</span>
+                </button>
+                <h2 class="project-title">TypeX Hub</h2>
+                <p class="project-description">Entrega do Projeto TypeX Hub</p>
+                <div class="divider"></div>
+                <div class="avatar-section">
+                    <div class="avatar"></div>
+                    <div class="user-info">
+                        <span class="user-name">Vitor Ferreira Viana</span>
+                        <span class="user-role">Diretor-Presidente</span>
+                    </div>
+                </div>
+                <button class="action-button">
+                    <div class="action-icon"></div>
+                </button>
+                <div class="edit-icon"></div>
+            </div>
+                        <div class="card">
+                <button class="status-button">
+                    <span class="status-text">Atrasado</span>
+                </button>
+                <h2 class="project-title">TypeX Hub</h2>
+                <p class="project-description">Entrega do Projeto TypeX Hub</p>
+                <div class="divider"></div>
+                <div class="avatar-section">
+                    <div class="avatar"></div>
+                    <div class="user-info">
+                        <span class="user-name">Vitor Ferreira Viana</span>
+                        <span class="user-role">Diretor-Presidente</span>
+                    </div>
+                </div>
+                <button class="action-button">
+                    <div class="action-icon"></div>
+                </button>
+                <div class="edit-icon"></div>
+            </div>
+        </div>
+        <button class="button_vermais">Ver mais</button>    
+    </div>
+</section>
+
 </body>
 </html>
