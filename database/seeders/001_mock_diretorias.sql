5db2e7c137cdfae5aba39f9fb7eecbfb93360a69
Vitor Ferreira Viana <vitorferreiraviana1703@gmail.com>
2025-09-07 16:18:55 -0300

    chore: ajustes gerais em rotas, header e arquivos desnecessários (#6)

--- database/seeders/001_mock_diretorias.sql
+++ database/seeders/001_mock_diretorias.sql
@@ -11,5 +11,6 @@ INSERT INTO `diretorias` (`diretoria_nome`,     `diretoria_desc`,               
 ('Financeiro',      'Responsável pela gestão financeira, planejamento orçamentário e controle de custos.',    '#1E90FF',         1),
 ('Recursos Humanos','Gerencia recrutamento, desenvolvimento de colaboradores e políticas de benefícios.',   '#32CD32',         1),
 ('Marketing',       'Planeja campanhas, comunicação institucional e estratégias de branding.',               '#FF8C00',         1),
-('Tecnologia',      'Desenvolvimento de sistemas, infraestrutura de TI e suporte a usuários.',               '#8A2BE2',         1),
-('Jurídico',        'Assessoria em questões legais, contratos e conformidade regulatória.',                     '#DC143C',         0);
+('Projetos',      'Desenvolvimento de sistemas, infraestrutura de TI e suporte a usuários.',               '#8A2BE2',         1),
+('Infraestrutura',  'Gerencia a infraestrutura física e tecnológica da empresa.',                     '#490037ff',         0),
+('Presidência',     'Coordenação das atividades da empresa e representação institucional.',                     '#ff0000ff',         1);
--- src/include/header.php
+++ src/include/header.php
@@ -1,33 +1,19 @@
-<?php
-// Calcula o caminho relativo para a pasta assets baseado na URL atual
-function obterAssetsPath() {
-    $uri = $_SERVER['REQUEST_URI'];
-    
-    if (strpos($uri, '/menu/') !== false) {
-        return '../../assets/';
-    }
-    
-    return '../assets/';
-}
-$assets_path = obterAssetsPath();
-?>
-
 <!DOCTYPE html>
 <html lang="PT-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TypeX Hub</title>
-    <link rel="stylesheet" href="<?php echo $assets_path; ?>css/vars.css">
-    <link rel="stylesheet" href="<?php echo $assets_path; ?>css/reset.css">
-    <link rel="stylesheet" href="<?php echo $assets_path; ?>css/global.css">
-    <link rel="stylesheet" href="<?php echo $assets_path; ?>css/header.css">
-    <link rel="stylesheet" href="<?php echo $assets_path; ?>css/sidebar.css">
-    <link rel="icon" href="<?php echo $assets_path; ?>images/tx-logo.ico" type="image/x-icon">
-    <script src="<?php echo $assets_path; ?>js/sidebar.js" defer></script>
+    <link rel="stylesheet" href="/assets/css/vars.css">
+    <link rel="stylesheet" href="/assets/css/reset.css">
+    <link rel="stylesheet" href="/assets/css/global.css">
+    <link rel="stylesheet" href="/assets/css/header.css">
+    <link rel="stylesheet" href="/assets/css/sidebar.css">
+    <link rel="icon" href="/assets/images/tx-logo.ico" type="image/x-icon">
+    <script src="/assets/js/sidebar.js" defer></script>
 </head>
 <body>
     <header>
-        <img src="<?php echo $assets_path; ?>images/tx-logo.png" alt="TX Logo">
+    <img src="/assets/images/tx-logo.png" alt="TX Logo">
         <h1>TypeX Hub</h1>
     </header>
