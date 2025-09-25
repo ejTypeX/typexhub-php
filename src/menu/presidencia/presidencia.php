2c896f3e0b0d02ce87a42633d56e5637aa425274
Lucas Furtado Souza <lucasfurtado256@gmail.com>
2025-09-25 17:41:19 -0300

    feat: HTML e CSS da Grid Reuniões - Presidência

--- src/menu/presidencia/presidencia.php
+++ src/menu/presidencia/presidencia.php
@@ -82,13 +82,13 @@ include "../../include/header.php";
     </div>
 </section>
 <section id="section_presidencia">
-    <div id="tasks_presidencia" class="section">
+    <div id="table_presidencia" class="section">
         <div class="header_section">
             <h1>Tasks (Diretoria)</h1>
             <button class="button_adicionar">+</button>
         </div>
-        <div id="table_tasks_mobile" class="container">
-            <table class="table_tasks">
+        <div id="table_presidencia_mobile" class="container">
+            <table class="table_presidencia">
                 <thead>
                     <th>Nome</th>
                     <th>Descrição</th>
@@ -182,6 +182,73 @@ include "../../include/header.php";
             </table>
         </div>
         
+        <button class="button_vermais" onclick="loadMoreProjects()">Ver mais</button>
+    </div>
+</section>
+<section id="section_presidencia">
+    <div id="reunioes_presidencia" class="section">
+        <div class="header_section">
+            <h1>Reuniões</h1>
+            <button class="button_adicionar">+</button>
+        </div>
+        <div id="table_presidencia_mobile" class="container">
+            <table class="table_presidencia table_presidencia_reuniao">
+                <thead>
+                    <th>Motivo</th>
+                    <th>Data</th>
+                    <th>Local</th>
+                    <th>Horário</th>
+                    <th>Descrição</th>
+                    <th>Status</th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="td_motivo">
+                            <div class="td-place-center">
+                                Alinhamento das Diretorias
+                            </div>
+                        </td>
+                        <td>20/07/2025</td>
+                        <td>Google Meeting</td>
+                        <td class="td_horario">06:00 - 23:00</td>
+                        <td class="td_descricao">Evento de empresas júniores do sudoeste do paraná</td>
+                        <td class="td-place-center"><span class="status-button concluido status-text">Concluído</span></td>
+                        <td>
+                            <button class="button_editar">
+                                <svg class="edit_icon" width="26" height="25" viewBox="0 0 26 25" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M23.563 1.43191C22.4977 0.420157 20.7757 0.420157 19.7104 1.43191L18.2462 2.81788L23.0085 7.34075L24.4726 5.95017C25.5379 4.93841 25.5379 3.30297 24.4726 2.29121L23.563 1.43191ZM9.00876 11.5957C8.71203 11.8775 8.48341 12.224 8.35207 12.6074L6.91221 16.7099C6.77114 17.1072 6.88302 17.5461 7.19434 17.8464C7.50567 18.1467 7.96778 18.2483 8.39098 18.1143L12.7106 16.7469C13.1094 16.6221 13.4743 16.405 13.7759 16.1232L21.914 8.38947L17.1469 3.86198L9.00876 11.5957ZM5.29237 3.38613C2.71424 3.38613 0.622559 5.37268 0.622559 7.82122V19.6481C0.622559 22.0967 2.71424 24.0832 5.29237 24.0832H17.7452C20.3233 24.0832 22.415 22.0967 22.415 19.6481V15.213C22.415 14.3953 21.7194 13.7347 20.8584 13.7347C19.9974 13.7347 19.3018 14.3953 19.3018 15.213V19.6481C19.3018 20.4659 18.6062 21.1265 17.7452 21.1265H5.29237C4.43137 21.1265 3.73577 20.4659 3.73577 19.6481V7.82122C3.73577 7.0035 4.43137 6.34286 5.29237 6.34286H9.96218C10.8232 6.34286 11.5188 5.68221 11.5188 4.86449C11.5188 4.04677 10.8232 3.38613 9.96218 3.38613H5.29237Z" fill="white"/></svg>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="td_motivo">Metas Futuras</td>
+                        <td>02/08/2025</td>
+                        <td>Av. Luiz Antônio, 1486</td>
+                        <td class="td_horario">11:00 - 00:00</td>
+                        <td class="td_descricao">Evento de empresas júniores do sudoeste do paraná</td>
+                        <td class="td-place-center"><span class="status-button andamento status-text">Programado</span></td>
+                        <td>
+                            <button class="button_editar">
+                                <svg class="edit_icon" width="26" height="25" viewBox="0 0 26 25" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M23.563 1.43191C22.4977 0.420157 20.7757 0.420157 19.7104 1.43191L18.2462 2.81788L23.0085 7.34075L24.4726 5.95017C25.5379 4.93841 25.5379 3.30297 24.4726 2.29121L23.563 1.43191ZM9.00876 11.5957C8.71203 11.8775 8.48341 12.224 8.35207 12.6074L6.91221 16.7099C6.77114 17.1072 6.88302 17.5461 7.19434 17.8464C7.50567 18.1467 7.96778 18.2483 8.39098 18.1143L12.7106 16.7469C13.1094 16.6221 13.4743 16.405 13.7759 16.1232L21.914 8.38947L17.1469 3.86198L9.00876 11.5957ZM5.29237 3.38613C2.71424 3.38613 0.622559 5.37268 0.622559 7.82122V19.6481C0.622559 22.0967 2.71424 24.0832 5.29237 24.0832H17.7452C20.3233 24.0832 22.415 22.0967 22.415 19.6481V15.213C22.415 14.3953 21.7194 13.7347 20.8584 13.7347C19.9974 13.7347 19.3018 14.3953 19.3018 15.213V19.6481C19.3018 20.4659 18.6062 21.1265 17.7452 21.1265H5.29237C4.43137 21.1265 3.73577 20.4659 3.73577 19.6481V7.82122C3.73577 7.0035 4.43137 6.34286 5.29237 6.34286H9.96218C10.8232 6.34286 11.5188 5.68221 11.5188 4.86449C11.5188 4.04677 10.8232 3.38613 9.96218 3.38613H5.29237Z" fill="white"/></svg>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="td_motivo">Novos Colaboradores</td>
+                        <td>30/10/2025</td>
+                        <td>Rua Borba Gato, 331</td>
+                        <td class="td_horario">7:00 - 22:00</td>
+                        <td class="td_descricao">Evento de empresas júniores do sudoeste do paraná</td>
+                        <td class="td-place-center"><span class="status-button nao-iniciado status-text">Cancelado</span></td>
+                        <td>
+                            <button class="button_editar">
+                                <svg class="edit_icon" width="26" height="25" viewBox="0 0 26 25" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M23.563 1.43191C22.4977 0.420157 20.7757 0.420157 19.7104 1.43191L18.2462 2.81788L23.0085 7.34075L24.4726 5.95017C25.5379 4.93841 25.5379 3.30297 24.4726 2.29121L23.563 1.43191ZM9.00876 11.5957C8.71203 11.8775 8.48341 12.224 8.35207 12.6074L6.91221 16.7099C6.77114 17.1072 6.88302 17.5461 7.19434 17.8464C7.50567 18.1467 7.96778 18.2483 8.39098 18.1143L12.7106 16.7469C13.1094 16.6221 13.4743 16.405 13.7759 16.1232L21.914 8.38947L17.1469 3.86198L9.00876 11.5957ZM5.29237 3.38613C2.71424 3.38613 0.622559 5.37268 0.622559 7.82122V19.6481C0.622559 22.0967 2.71424 24.0832 5.29237 24.0832H17.7452C20.3233 24.0832 22.415 22.0967 22.415 19.6481V15.213C22.415 14.3953 21.7194 13.7347 20.8584 13.7347C19.9974 13.7347 19.3018 14.3953 19.3018 15.213V19.6481C19.3018 20.4659 18.6062 21.1265 17.7452 21.1265H5.29237C4.43137 21.1265 3.73577 20.4659 3.73577 19.6481V7.82122C3.73577 7.0035 4.43137 6.34286 5.29237 6.34286H9.96218C10.8232 6.34286 11.5188 5.68221 11.5188 4.86449C11.5188 4.04677 10.8232 3.38613 9.96218 3.38613H5.29237Z" fill="white"/></svg>
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
         
         <button class="button_vermais" onclick="loadMoreProjects()">Ver mais</button>
     </div>
