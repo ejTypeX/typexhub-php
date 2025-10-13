7c51348e08aef7b7c771cfe461a47260e8864136
Maikon-icaro <137800939+Maikon-sant@users.noreply.github.com>
2025-10-13 20:14:32 -0300

    Criação da Página de Projetos e Página de Detalhes do Projeto (#13)
    * Design Página de projetos
    
    * Design Página de projetos (Ajuste de configurações iniciais)

--- src/include/header.php
+++ src/include/header.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html lang="PT-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TypeX Hub</title>
-    <link rel="stylesheet" href="/assets/css/vars.css">
-    <link rel="stylesheet" href="/assets/css/reset.css">
-    <link rel="stylesheet" href="/assets/css/global.css">
-    <link rel="stylesheet" href="/assets/css/header.css">
-    <link rel="stylesheet" href="/assets/css/sidebar.css">
-    <link rel="icon" href="/assets/images/tx-logo.ico" type="image/x-icon">
+    <link rel="stylesheet" href="../../assets/css/vars.css">
+    <link rel="stylesheet" href="../../assets/css/reset.css">
+    <link rel="stylesheet" href="../../assets/css/global.css">
+    <link rel="stylesheet" href="../../assets/css/header.css">
+    <link rel="stylesheet" href="../../assets/css/sidebar.css">
+    <!-- Style Sessão Projetos -->
+    <link rel="stylesheet" href="../../assets/css/projetos.css">
+    <!-- Bootstrap Icons -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
+    <link rel="icon" href="../../assets/images/tx-logo.ico" type="image/x-icon">
