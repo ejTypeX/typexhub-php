f671d311cf05741a9087b8bcd79d5c72e53056bf
Caetano Pavan <caetanopavan0@gmail.com>
2025-10-22 20:17:56 -0300

    feat: registro

--- src/include/conexao.php
+++ src/include/conexao.php
@@ -1,21 +1,17 @@
 <?php
-
 $host = getenv('DB_HOST');
 $db   = getenv('DB_NAME');
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASS');
 $charset = 'utf8mb4';
-
 $dsn = "mysql:host=$host;port=3306;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
 ];
-
 try {
     $pdo = new PDO($dsn, $user, $pass, $options);
-    echo 'Conexão bem-sucedida ao banco de dados!';
 } catch (PDOException $e) {
     echo 'Erro de conexão: ' . $e->getMessage();
     exit;
